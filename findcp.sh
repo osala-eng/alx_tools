dc3a7d9dfa6e0cdfaf46473ed5a1d661b7399c74
Jashon Osala <jashonosala@gmail.com>
2022-07-22 19:16:26 +0300

    Modified script

--- findcp.sh
+++ findcp.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ "$#" -eq 0 ]; then
-	echo -e "No arguments given..."
-	exit 1
+        echo -e "No arguments given..."
+        exit 1
 fi
 
 # collect args
 
 for arg in "$@"; do
-	__line="`echo $arg | sed -e 's/.o/.c/g'`"
-	cp "`find ../ -name ${__line} | sed -n '1p'`" .
+        while read line; do
+                __line="`echo $line | sed 's/\.o/\.c/g' | tr -d '\n' `"
+                cp "`find ../ -name $__line | sed -n '1p' `" .
+        done < $arg
 done
-
-clear && ls
