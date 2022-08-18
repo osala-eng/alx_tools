7fdbee6dfce570c047cc780d9c03df5dda8d0ec5
Jashon Osala <jashonosala@gmail.com>
2022-08-19 01:40:43 +0300

    Done

--- crackme3.sh
+++ crackme3.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Get pass script
+echo -e
+echo -e "Fetching start file ..."
+echo -e
 
 curl https://raw.githubusercontent.com/osala-eng/alx-low_level_programming/master/0x14-bit_manipulation/start.sh -o start.xt
 
